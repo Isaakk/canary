5f9404319572c2c539a2f7a4f49557ed2d8b02ad
Isaakk <34930439+Isaakk@users.noreply.github.com>
2024-05-27 20:16:42 +0200

    Update door.lua

--- data-otservbr-global/scripts/quests/the_annihilator/door.lua
+++ data-otservbr-global/scripts/quests/the_annihilator/door.lua
@@ -1,11 +1,6 @@
 local door = Action()
 
 function door.onUse(player, item, fromPosition, target, toPosition, isHotkey)
-	if player:getStorageValue(Storage.Quest.U7_24.TheAnnihilator.Reward) == 1 then
-		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "The door seems to be sealed against unwanted intruders.")
-		return true
-	end
-
 	if item.itemid == 5113 then
 		player:teleportTo(toPosition, true)
 		item:transform(item.itemid + 1)
